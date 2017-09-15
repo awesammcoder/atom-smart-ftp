73a843c32aa64eaa57826d013e783b963ad68522
Sammuel Lagat Apa <apa.sammuel@gmail.com>
2017-09-15 20:43:56 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SmartFTP
+
+An atom ftp package with realtime event watcher for files and directories indicator.
+
+> Status: On Development
+
+## Installation
+>
+
+## Contributors
+>
---
+++ spec/smart-ftp-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SmartFtpView from '../lib/smart-ftp-view';
+
+describe('SmartFtpView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
